f777cb414d4e3129bae193250d1b1801fdfa42ac
Carlassanchez24 <carlassanchez24@gmail.com>
2024-09-19 11:21:58 +0200

    DONE: users token, serilizer, models, views, TODO: migrations, test

---
+++ users/views.py
@@ -0,0 +1,23 @@
+from .models import CustomUser
+from rest_framework import generics, permissions, views, status
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class LogoutView(views.APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        try:
+            token = Token.objects.get(user=request.user)
+            token.delete()
+            return Response({"message": "Token deleted"}, status=status.HTTP_200_OK)
+        except Token.DoesNotExist:
+            return Response({"message": "Token not found"}, status=status.HTTP_400_BAD_REQUEST)
