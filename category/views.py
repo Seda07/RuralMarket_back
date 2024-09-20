ac8033796f73ba216db9af2ff849a8abd02da2fb
GabyRosas <fleuryrosas@gmail.com>
2024-09-20 14:51:10 +0200

    DONE: API category

--- category/views.py
+++ category/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import CategorySerializer
+from .models import Category
 
-# Create your views here.
+
+class CategoryView(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
