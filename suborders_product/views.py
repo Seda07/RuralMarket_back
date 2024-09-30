e2c67b50d7a638dc6a427d5c0c717f11be137619
Carlassanchez24 <carlassanchez24@gmail.com>
2024-09-30 10:46:24 +0200

    DONE: suborderproduct models views and serializer, TODO: validations

--- suborders_product/views.py
+++ suborders_product/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .serializer import SuborderProductSerializer
+from .models import SuborderProduct
 
-# Create your views here.
+
+class SuborderProduct(viewsets.ModelViewSet):
+    serializer_class = SuborderProductSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return SuborderProduct.objects.filter(user=self.request.user)
