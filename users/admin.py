185b3ba4a60f90218e42d9d5774bd67189eb9e4a
GabyRosas <fleuryrosas@gmail.com>
2024-09-30 15:56:48 +0200

    DONE: reviewsModels & bugfix customUser

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+# Registrar el modelo CustomUser con la configuración de UserAdmin
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    # Si quieres personalizar los campos visibles en la vista de admin:
+    search_fields = ('username', 'email')
+
