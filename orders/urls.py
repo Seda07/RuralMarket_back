339fe24c66fee45778a612c6ee1f32c50d5b601e
GabyRosas <fleuryrosas@gmail.com>
2024-10-07 14:51:58 +0200

    DONE:Suborder & suborder_products

--- orders/urls.py
+++ orders/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import OrderView
+from .views import CreateOrderView, OrderSellerView
 
 router = DefaultRouter()
-router.register(r'order', OrderView, basename='order')
+router.register(r'order', CreateOrderView, basename='order')
 
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('create/', CreateOrderView.as_view({'post': 'create'}), name='create-order'),
+    path('seller/', OrderSellerView.as_view({'get': 'list'}), name='order-seller'),
 ]
+
